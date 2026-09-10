7c890731e4de0e9582918d8358b70407e45ac6ca
Иванов Андрей <iv4noff2008@ya.ru>
2026-09-11 02:35:40 +0300

    Блок 2. Проверка корректности и преобразование исходных данных в целые числа

--- mathtool.py
+++ mathtool.py
@@ -1,6 +1,6 @@
 import sys
 
-if len(sys.argv) == 0 or sys.argv[1] == '--help':
+if len(sys.argv) == 1 or sys.argv[1] == '--help':
     print('''mathtool — решение уравнений вида A*x^2 + B*x + C = 0
 
 Использование:
@@ -13,14 +13,42 @@ if len(sys.argv) == 0 or sys.argv[1] == '--help':
 ''')
     sys.exit('0')
 
-elif sys.argv[1] == '--solve' and len(sys.argv) == 1:
-    a = int(input('Введите коэффициент A: '))
-    b = int(input('Введите коэффициент B: '))
-    c = int(input('Введите коэффициент C: '))
-elif sys.argv[1] == '--solve' and len(sys.argv) == 4:
-    a = int(sys.argv[2])
-    b = int(sys.argv[3])
-    c = int(sys.argv[4])
+elif sys.argv[1] == 'solve' and len(sys.argv) == 2:
+    a = str(input('Введите коэффициент A: '))
+    if a.isdigit():
+        a = int(a)
+    else:
+        print('Недопустимый ввод')
+        sys.exit('1')
+    b = str(input('Введите коэффициент B: '))
+    if b.isdigit():
+        b = int(b)
+    else:
+        print('Недопустимый ввод')
+        sys.exit('1')
+    c = str(input('Введите коэффициент C: '))
+    if c.isdigit():
+        c = int(c)
+    else:
+        print('Недопустимый ввод')
+        sys.exit('1')
+    
+elif sys.argv[1] == 'solve' and len(sys.argv) == 4:
+    if str(sys.argv[2]).isdigit():
+        a = int(sys.argv[2])
+    else:
+        print('Недопустимый ввод')
+        sys.exit('1')
+    if str(sys.argv[3]).isdigit():
+            b = int(sys.argv[3])
+    else:
+        print('Недопустимый ввод')
+        sys.exit('1')
+    if str(sys.argv[4]).isdigit():
+            c = int(sys.argv[4])
+    else:
+        print('Недопустимый ввод')
+        sys.exit('1')
 
 else:
     print('Недопустимый ввод')

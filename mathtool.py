7504f58b2f76bdcc7652539af1eda960fd7011a3
Иванов Андрей <iv4noff2008@ya.ru>
2026-09-11 01:38:28 +0300

    Блок 1. Ввод данных

--- mathtool.py
+++ mathtool.py
@@ -1,8 +1,27 @@
 import sys
 
 if len(sys.argv) == 0 or sys.argv[1] == '--help':
-    print('Всё ок')
+    print('''mathtool — решение уравнений вида A*x^2 + B*x + C = 0
+
+Использование:
+    python mathtool.py                         вывод справки
+    python mathtool.py --help                  вывод справки
+    python mathtool.py solve                   ввод коэффициентов с клавиатуры
+    python mathtool.py solve -a 1 -b -3 -c 2   решение с заданными коэффициентами
+
+Коэффициенты A, B, C — целые числа, по модулю не превышающие 10000.
+''')
     sys.exit('0')
+
+elif sys.argv[1] == '--solve' and len(sys.argv) == 1:
+    a = int(input('Введите коэффициент A: '))
+    b = int(input('Введите коэффициент B: '))
+    c = int(input('Введите коэффициент C: '))
+elif sys.argv[1] == '--solve' and len(sys.argv) == 4:
+    a = int(sys.argv[2])
+    b = int(sys.argv[3])
+    c = int(sys.argv[4])
+
 else:
-    print('Всё не ок')
+    print('Недопустимый ввод')
     sys.exit('1')
